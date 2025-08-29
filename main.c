96ec23ae040dd7c0112100eb988151fe77aae47c
any772 <hideshatoshi@gmail.com>
2025-08-29 12:20:09 +0000

    grammar fix

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello! My name is Erwin Junanto and I love to use C language, because it's super fast!\n");
+	printf("Hello! My name is Erwin Junanto and I love to use the C language, it's super fast!\n");
 	return 0;
 }
